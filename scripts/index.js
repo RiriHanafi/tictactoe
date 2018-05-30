2678b69eea67799488cb309b644e4c7cc12965db
Riri Hanafi <de2via@gmail.com>
2018-05-30 21:57:27 +0700

    Update index js.

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
-var containerGame = document.getElementById('containerGame')
+var containerGame = document.getElementById('containerGame');
+var player = "X";
 
 function generateBoard (){
     containerGame.innerHTML = ''
@@ -12,12 +13,46 @@ function generateBoard (){
             
         for (j=0; j < jumlahKolom; j++){
             var divKolom = document.createElement('DIV')
-            divKolom.id = 'box'+i+j
+            divKolom.id = 'box-'+i+j
             divKolom.className = 'box'
-                    
+            divKolom.addEventListener("click", turn)
             divBaris.appendChild(divKolom)
         }
         
     containerGame.appendChild(divBaris)
+    }
+}
+
+function turn(){
+    var position = this.id.split('-')[1]    
+    var winPatternX = [];
+    var winPatternO = [];
+
+    for (var i = position; i < 9; i++) {
+    if (this.innerHTML) {
+        return
+    }
+    if (player === "X"){
+        this.innerHTML = "X";
+        console.log(i);
+        winPatternX.push(i);
+        console.log(winPatternX);
+
+        //positionX = position[0];
+        //positionY = position[1];
+        //console.log("X: ", positionX);
+        //console.log("Y: ", positionY);
+        player = "O";
+    } else {
+        this.innerHTML = "O";
+        console.log(i);
+        winPatternO.push(i);
+        console.log(winPatternO);
+        //positionX = position[0];
+        //positionY = position[1];
+        //console.log("X: ", positionX);
+        //console.log("Y: ", positionY);
+        player = "X";
+    }
     }
 }

84da01184c4f1a31f3103b1154c1fde7d5ff601a
Riri Hanafi <de2via@gmail.com>
2018-06-05 19:00:26 +0700

    Update index html & js.

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,12 @@
 var containerGame = document.getElementById('containerGame');
+var msg = document.getElementById("message");
 var player = "X";
+
+function startGame() {
+    myGameArea.start();
+}
+
+var myGame = generateBoard();
 
 function generateBoard (){
     containerGame.innerHTML = ''
@@ -23,36 +30,64 @@ function generateBoard (){
     }
 }
 
+
+
+var movePattern = [ '00', '10', '20', '01', '11', '21', '02', '12', '22']
+var countMove = 0;
 function turn(){
     var position = this.id.split('-')[1]    
-    var winPatternX = [];
-    var winPatternO = [];
+    
+    for (var i = 0; i < 9; i++) {
+        if (this.innerHTML) {
+            return
+        }   
+        
+        if (player === "X"){
+            this.innerHTML = "X";
+            var x = movePattern.indexOf(position);
+            console.log(position);
+            movePattern.splice(x, 1, "X");
+            console.log(movePattern);
+            countMove++;
+            console.log(countMove);
+            player = "O";
+        } else {
+            this.innerHTML = "O";
+            var x = movePattern.indexOf(position);
+            console.log(position);
+            movePattern.splice(x, 1, "O");
+            console.log(movePattern);
+            countMove++;
+            console.log(countMove);
+            player = "X";
+        }
 
-    for (var i = position; i < 9; i++) {
-    if (this.innerHTML) {
-        return
+        if (countMove >= 5){
+            checkWin();
+        }
     }
-    if (player === "X"){
-        this.innerHTML = "X";
-        console.log(i);
-        winPatternX.push(i);
-        console.log(winPatternX);
+}
 
-        //positionX = position[0];
-        //positionY = position[1];
-        //console.log("X: ", positionX);
-        //console.log("Y: ", positionY);
-        player = "O";
-    } else {
-        this.innerHTML = "O";
-        console.log(i);
-        winPatternO.push(i);
-        console.log(winPatternO);
-        //positionX = position[0];
-        //positionY = position[1];
-        //console.log("X: ", positionX);
-        //console.log("Y: ", positionY);
-        player = "X";
-    }
+function checkWin(){
+    console.log(`==>`, movePattern);
+    if (movePattern[0] === movePattern[1] && movePattern[0] === movePattern[2] && movePattern[1] === movePattern[2]){
+        console.log(movePattern[0] + `'s win!`);
+        player = movePattern[0];
+        msg.innerHTML = player + `'s win!`;
+        starGame();
+    } else if (movePattern[3] === movePattern[4] && movePattern[3] === movePattern[5] && movePattern[4] === movePattern[5]){
+        alert(movePattern[3] + `'s win!`);
+    } else if (movePattern[6] === movePattern[7] && movePattern[6] === movePattern[8] && movePattern[7] === movePattern[8]){
+        alert(movePattern[0] + `'s win!`);
+    } else if (movePattern[0] === movePattern[3] && movePattern[0] === movePattern[6] && movePattern[3] === movePattern[6]){
+        alert(movePattern[0] + `'s win!`);
+    } else if (movePattern[1] === movePattern[4] && movePattern[1] === movePattern[7] && movePattern[4] === movePattern[7]){
+        alert(movePattern[0] + `'s win!`);
+    } else if (movePattern[2] === movePattern[5] && movePattern[2] === movePattern[8] && movePattern[5] === movePattern[8]){
+        alert(movePattern[0] + `'s win!`);
+    } else if (movePattern[0] === movePattern[4] && movePattern[0] === movePattern[8] && movePattern[4] === movePattern[7]){
+        alert(movePattern[0] + `'s win!`);
+    } else if (movePattern[2] === movePattern[4] && movePattern[2] === movePattern[6] && movePattern[4] === movePattern[6]){
+        alert(movePattern[0] + `'s win!`);
     }
 }
